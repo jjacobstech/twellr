8254bb7b56cdae96c808dcd8ae1189a49d75b14d
jjacobstech <jacobsjoshua81@gmail.com>
2025-03-31 20:12:34 +0100

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-use Livewire\Features\SupportFileUploads\FileUploadConfiguration;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,5 +18,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void {}
+    public function boot(): void
+    {
+        URL::forceScheme('https');
+    }
 }
