0a62b20a7982b2cdf353ad6df0e0c33a89eea67b
jjacobstech <jacobsjoshua81@gmail.com>
2025-03-31 18:01:06 +0100

    style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/assets/app-CY9V-t46.css') }}">
 </head>
 
 <body class="h-screen p-0 m-0 font-sans antialiased bg-white border-0 md:overflow-hidden">
@@ -46,6 +47,7 @@
         </main>
     </div>
 </body>
+<script src="{{ asset('build/assets/app-Cc28pn9L.js') }}"></script>
 <script src="{{ asset('vendor/bladewind/js/helpers.js') }}"></script>
 
 </html>
