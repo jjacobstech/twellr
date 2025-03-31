581ce13cc3a8474f468a97663b27bc3aa1cf4e89
jjacobstech <jacobsjoshua81@gmail.com>
2025-03-31 16:59:57 +0100

    Style conflict resolution

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 
 
 export default defineConfig({
+    base: '/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
