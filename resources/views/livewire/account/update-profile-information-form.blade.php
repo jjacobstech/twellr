957ae383ab4bef4a8ac5cd4f9ab22526e3ce1f1d
jjacobstech <jacobsjoshua81@gmail.com>
2025-04-10 21:46:44 +0100

    cover upload change

--- resources/views/livewire/account/update-profile-information-form.blade.php
+++ resources/views/livewire/account/update-profile-information-form.blade.php
@@ -439,24 +439,6 @@ new class extends Component {
                                             JPG, PNG, GIF up to 2MB
                                         </p>
                                     </div>
-                                    <div x-show="remove" class="space-y-1 text-center">
-                                        <svg class="w-12 h-12 mx-auto text-gray-500" stroke="currentColor"
-                                            fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                                            <path
-                                                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                        </svg>
-                                        <div class="flex justify-center text-sm text-gray-600">
-                                            <label for="cover-image-upload"
-                                                class="relative flex justify-center font-medium text-center rounded-md cursor-pointer text-navy-blue hover:text-gray-500 focus-within:outline-none ">
-                                                <span>Upload a file</span>
-                                            </label>
-
-                                        </div>
-                                        <p class="text-xs text-gray-500">
-                                            JPG, PNG, GIF up to 2MB
-                                        </p>
-                                    </div>
                                 @endif
                                 <x-mary-file @click="remove = !remove" wire:model="coverUpload"
                                     accept="image/png, image/jpeg, image/jpg">
@@ -465,6 +447,7 @@ new class extends Component {
                                         src="{{ Auth::user()->cover ? asset('uploads/cover/' . Auth::user()->cover) : '' }}"
                                         class="@if (Auth::user()->cover) border-2 border-gray-500 border-dashed rounded-md hover:border-navy-blue p-2 @endif">
                                 </x-mary-file>
+
 
                             </label>
                         </div>
