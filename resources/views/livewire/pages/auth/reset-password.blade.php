23e05c67ae245b70aa1cf52ea69be6c43da7b530
jjacobstech <jacobsjoshua81@gmail.com>
2025-05-17 22:34:03 +0100

    password reset

--- resources/views/livewire/pages/auth/reset-password.blade.php
+++ resources/views/livewire/pages/auth/reset-password.blade.php
@@ -70,31 +70,46 @@ new #[Layout('layouts.guest')] class extends Component
 }; ?>
 
 <div>
+
+
+
+
     <form wire:submit="resetPassword">
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+       <!-- Email Address -->
+            <div class="relative w-100">
+                <x-input-label class="absolute z-50 px-1 ml-3 font-extrabold bg-white mt-7" for="email"
+                    :value="__('Email')" />
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input wire:model="password" id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                <div class="absolute w-full mt-4">
+                    <x-text-input wire:model="email" id="email" class="block w-full mt-5" type="email"
+                        name="email" required autofocus autocomplete="email" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2 validator absolute" />
+                </div>
+            </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <!-- Password -->
+            <div class="relative mt-20 w-100">
+                <x-input-label class="absolute z-50 px-1 ml-3 font-extrabold bg-white mt-7" for="password"
+                    :value="__('Password')" />
 
-            <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
-                          type="password"
-                          name="password_confirmation" required autocomplete="new-password" />
+                <div class="absolute w-full mt-4">
+                    <x-text-input wire:model="password" id="password" class="block w-full mt-5" type="password"
+                        name="password" required autofocus autocomplete="password" />
+                    <x-input-error :messages="$errors->get('password')" class="mt-2 validator absolute" />
+                </div>
+            </div>
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+         <!--  Confirm Password -->
+            <div class="relative mt-20 w-100">
+                <x-input-label class="absolute z-50 px-1 ml-3 font-extrabold bg-white mt-7" for="password_confirmation"
+                    :value="__('Confirm Password')" />
+
+                <div class="absolute w-full mt-4">
+                    <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block w-full mt-5" type="password_confirmation"
+                        name="password_confirmation" required autofocus autocomplete="password_confirmation" />
+                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2 validator absolute" />
+                </div>
+            </div>
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
