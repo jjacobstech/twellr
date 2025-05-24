d9a54293e40538ab8ca73af110492a204892aeaf
jjacobstech <jacobsjoshua81@gmail.com>
2025-05-24 19:40:33 +0100

    designer profilr fix

--- resources/views/livewire/pages/creative/profile.blade.php
+++ resources/views/livewire/pages/creative/profile.blade.php
@@ -90,21 +90,21 @@ $toggleFollow = action(function () {
 <div class="w-[100%] overflow-y-scroll px-8 md:px-16 pb-20 h-screen bg-gray-100 scrollbar-none">
     <!-- Profile Cover Section -->
     <div class="w-full">
-        <div style="background-image: url('@if (empty(Auth::user()->cover)) {{ asset('assets/pexels-solliefoto-298863.jpg') }}@else{{ asset('uploads/cover/' . Auth::user()->cover) }} @endif')"
+        <div style="background-image: url('@if (empty($user->cover)) {{ asset('assets/pexels-solliefoto-298863.jpg') }}@else{{ asset('uploads/cover/' . $user->cover) }} @endif')"
             class="rounded-[14px] bg-no-repeat bg-cover bg-center justify-center items-center w-full
                    mt-4 sm:mt-8 md:mt-12 lg:mt-16 xl:mt-20
                    min-h-[180px] sm:min-h-[200px] md:min-h-[220px] lg:min-h-[250px] xl:min-h-[280px] pt-1">
 
             <div class="relative flex justify-center w-full">
-                <img class="absolute z-10 object-cover {{ !empty(Auth::user()->avatar) && str_contains(Auth::user()->avatar, 'https://') ? 'aspect-square' : '' }}
+                <img class="absolute z-10 object-cover {{ !empty($user->avatar) && str_contains($user->avatar, 'https://') ? 'aspect-square' : '' }}
                          w-16 h-16 mt-10
                          sm:w-18 sm:h-18 sm:mt-12
                          md:w-20 md:h-20 md:mt-16
                          lg:w-32 lg:h-32 lg:mt-20
                          xl:w-40 xl:h-40 xl:mt-28
                          rounded-full border-2 sm:border-3 md:border-4 border-white shadow-lg"
-                    src="@if (!empty(Auth::user()->avatar) && str_contains(Auth::user()->avatar, 'https://')) {{ Auth::user()->avatar }}@elseif(!empty(Auth::user()->avatar)){{ asset('uploads/avatar/' . Auth::user()->avatar) }}@else{{ asset('assets/icons-user.png') }} @endif"
-                    alt="{{ Auth::user()->name }}" />
+                    src="@if (!empty($user->avatar) && str_contains($user->avatar, 'https://')) {{ $user->avatar }}@elseif(!empty($user->avatar)){{ asset('uploads/avatar/' . $user->avatar) }}@else{{ asset('assets/icons-user.png') }} @endif"
+                    alt="{{ $user->name }}" />
             </div>
 
             <div
@@ -115,7 +115,7 @@ $toggleFollow = action(function () {
                       lg:mt-28 lg:py-9 lg:px-10
                       xl:mt-48 xl:py-9 xl:px-12">
 
-                @if (Auth::user()->isCreative())
+              @if (Auth::check() && Auth::id() !== $user->id)
                     <div
                         class="grid items-center justify-center
                                 sm:mt-8 md:mt-10 lg:mt-12 xl:mt-16 space-y-3">
@@ -157,18 +157,9 @@ $toggleFollow = action(function () {
                     <h1
                         class="font-bold text-gray-500 leading-tight break-words px-2
                              my-2 text-sm sm:text-base md:text-lg lg:text-2xl xl:text-3xl">
-                        {{ Auth::user()->firstname }} {{ Auth::user()->lastname }}
+                        {{ $user->firstname }} {{ $user->lastname }}
                     </h1>
-                @else
-                    <h1
-                        class="font-bold text-gray-500 leading-tight break-words px-2
-                             my-8 text-sm
-                             sm:my-10 sm:text-base
-                             md:my-12 md:text-lg
-                             lg:my-14 lg:text-2xl
-                             xl:my-16 xl:text-3xl">
-                        {{ Auth::user()->firstname }} {{ Auth::user()->lastname }}
-                    </h1>
+
                 @endif
                 @if (Auth::check() && Auth::id() !== $user->id)
                     <div class="mt-2 mb-4">
