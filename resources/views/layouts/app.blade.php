ca222c8a2bd49eddab09dcc7a2045c1508af1931
jjacobstech <jacobsjoshua81@gmail.com>
2025-05-24 18:32:40 +0100

    screens

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,21 @@
 <body class="fixed w-screen h-screen font-sans antialiased bg-white">
     <x-mary-toast position="toast-top top-right" />
 
+    <div wire:offline
+        class="fixed top-4 right-4 z-[9999] w-[90%] max-w-sm sm:max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg"
+        x-data="{ show: true }" x-show="show" x-transition:leave="transition ease-in duration-300"
+        x-transition:leave-start="opacity-100 transform" x-transition:leave-end="opacity-0 -translate-y-2">
+        <div
+            class="flex items-center justify-between p-4 text-sm font-semibold text-white bg-navy-blue rounded-xl shadow-lg">
+            <span>Offline</span>
+            <button @click="show = false" class="ml-4 focus:outline-none">
+                @svg('eva-close', 'w-5 h-5 text-red-400 hover:text-red-500')
+            </button>
+        </div>
+    </div>
+
+  
+
     <div wire:loading.class='hidden' class="h-full bg-gray-100">
         {{-- To Allow Admin view blog, marketplace and explore pages from admin panel --}}
         @if (Auth::user()->role === 'admin' || Auth::user()->role == 'admin')
@@ -47,11 +62,11 @@
             x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100">
             {{ $slot }}
         </main>
-        <div class="fixed bottom-0 left-0 right-0 z-50 bg-white shadow-sm">
+        <div class="fixed bottom-0 left-0 right-0 z-50 bg-transparent shadow-sm">
             <x-footer />
         </div>
     </div>
- {{-- <!--Start of Tawk.to Script
+    {{-- <!--Start of Tawk.to Script
     -->
         <script type="text/javascript">
             let customerSupport = document.getElementById('customer_support');
